be6eaf710b4156a193cf85572f76ef10ff35df26
rb-cesar <rbcesar86@gmail.com>
2024-05-09 22:48:37 -0300

    :bookmark: releases: Change emitter method 'remove' to 'off', update types and hide internal methods

--- types/emitter.d.ts
+++ types/emitter.d.ts
@@ -2,7 +2,7 @@ export type EmitterEventsMetadata<T> = {
   [P in keyof T]: { key: string; execute: (args: T[P]) => void }[];
 };
 
-export type OnRegisterFunc<T> = <P extends keyof T>(name: P, ...rest: ((args: T[P]) => void)[]) => string[];
+export type RegisterFunc<T> = <P extends keyof T>(name: P, ...rest: ((args: T[P]) => void)[]) => string[];
 
 export type EmitFunc<T> = <P extends keyof T>(name: P, args?: T[P]) => void;
 
@@ -16,13 +16,25 @@ export type AddNewEventsInToListFunc<T> = <P extends keyof T>(
   eventCalls: { key: string; execute: (args: T[P]) => void }[],
 ) => void;
 
-export type DeleteEventFunc<T> = <P extends keyof T>(name: P, key?: string) => void;
+export type UnregisterFunc<T> = <P extends keyof T>(name: P, key?: string) => void;
 
-export interface IEmitter<T = any> {
-  on: OnRegisterFunc<T>;
+export interface IEmitterObj<T = any> {
+  on: RegisterFunc<T>;
+  off: UnregisterFunc<T>;
   emit: EmitFunc<T>;
   events: EmitterEventsMetadata<T> | null;
   createNewEventList: CreateNewEventListFunc<T>;
   addNewEventsInList: AddNewEventsInToListFunc<T>;
-  remove: DeleteEventFunc<T>;
+  remove: UnregisterFunc<T>;
+}
+
+export interface IEmitter<T = any> {
+  on: RegisterFunc<T>;
+  off: UnregisterFunc<T>;
+  emit: EmitFunc<T>;
+  events: EmitterEventsMetadata<T> | null;
+  /**
+    * @deprecated The method should not be used, use "off" instead of this method
+  */
+  remove: UnregisterFunc<T>;
 }
