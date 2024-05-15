a161de3fbdc1903bf0cd2dc4477628b7190717d6
rb-cesar <rbcesar86@gmail.com>
2024-05-15 08:50:50 -0500

    :sparkles: feat: Add scroll observer

---
+++ hooks/useScrollTargetObserver.ts
@@ -0,0 +1,82 @@
+import { useEffect, useRef, useState } from 'react';
+
+export type ObserverCallbackOptions = {
+  entry: IntersectionObserverEntry;
+  hasScrollY: boolean;
+  hasScrollX: boolean;
+};
+
+export type RootCheckScrollOptions = Omit<ObserverCallbackOptions, 'entry'>;
+
+export type RootScrollOptions = {
+  ev: Event;
+};
+
+export type ScrollObserverOptions = {
+  rootElement?: Element | null;
+  targetElement?: Element | null;
+  threshold?: number;
+  rootMargin?: string;
+  checkScroll?: (options: RootCheckScrollOptions) => void;
+  onScroll?: (options: RootScrollOptions) => void;
+  onEnter?(options: ObserverCallbackOptions): void;
+  onLeave?(options: ObserverCallbackOptions): void;
+};
+
+export function useScrollTargetObserver(options: ScrollObserverOptions) {
+  const { rootElement, targetElement, threshold = 0.5, rootMargin, onEnter, onLeave, onScroll, checkScroll } = options;
+
+  const rootElementResizeRef = useRef<{
+    timeout: NodeJS.Timeout | null;
+    delayMs: number;
+  }>({
+    timeout: null,
+    delayMs: 50,
+  });
+
+  useEffect(() => {
+    if (!rootElement || !targetElement) return;
+
+    const handleScroll = (ev: Event) => {
+      onScroll?.({ ev });
+    };
+
+    rootElement.addEventListener('scroll', handleScroll);
+
+    const resizeObserver = new ResizeObserver(() => {
+      if (!checkScroll) return;
+
+      clearTimeout(rootElementResizeRef.current.timeout!);
+
+      rootElementResizeRef.current.timeout = setTimeout(() => {
+        const hasScrollY = rootElement.scrollHeight > rootElement.clientHeight;
+        const hasScrollX = rootElement.scrollWidth > rootElement.clientWidth;
+
+        checkScroll({ hasScrollY, hasScrollX });
+      }, rootElementResizeRef.current.delayMs);
+    });
+
+    const observer = new IntersectionObserver(
+      entries => {
+        const options: ObserverCallbackOptions = {
+          entry: entries[0],
+          hasScrollX: rootElement.scrollWidth > rootElement.clientWidth,
+          hasScrollY: rootElement.scrollHeight > rootElement.clientHeight,
+        };
+
+        if (entries[0].isIntersecting) onEnter?.(options);
+        else onLeave?.(options);
+      },
+      { root: rootElement, threshold, rootMargin },
+    );
+
+    observer.observe(targetElement);
+    resizeObserver.observe(rootElement);
+
+    return () => {
+      observer.unobserve(targetElement);
+      resizeObserver.unobserve(rootElement);
+      rootElement.removeEventListener('scroll', handleScroll);
+    };
+  }, [rootElement, targetElement, threshold, rootMargin]); // eslint-disable-line react-hooks/exhaustive-deps
+}
