47dae5c17faf71b81edc59b8739b0653c5689a98
gmendesfonseca <fonseca.gabriel.mendes@gmail.com>
2025-04-23 13:09:50 -0300

    refactor: :recycle: replace moment.js with luxon for date handling

--- utils/dateFormat.ts
+++ utils/dateFormat.ts
@@ -1,4 +1,4 @@
-import moment from 'moment';
+import { DateTime } from 'luxon';
 import { getDefaultLanguage } from './getDefaultLanguage';
 
 export const dateFormat = (date: string | Date, format: string) => {
@@ -7,10 +7,10 @@ export const dateFormat = (date: string | Date, format: string) => {
    *
    * params:
    *  date: a valid string date format or a javascript date object
-   *  format: string representing the format for date string return (must be an moment valid format)
+   *  format: string representing the format for date string return (must be a Luxon valid format)
    *
-   * return: formated date as string
+   * return: formatted date as string
    */
   const default_language = getDefaultLanguage();
-  return moment(date).locale(default_language).format(format);
+  return DateTime.fromJSDate(new Date(date)).setLocale(default_language).toFormat(format);
 };
