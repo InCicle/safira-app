3a94f1898f7d26ad0162dfbf71a398c79dc27aba
gmendesfonseca <fonseca.gabriel.mendes@gmail.com>
2025-06-05 15:48:00 -0300

    refactor: :recycle: simplify hasManagerPermissions function for better readability

--- utils/hasManagerPanel.ts
+++ utils/hasManagerPanel.ts
@@ -6,11 +6,10 @@ export function hasManagerPermissions(
   checkPermission: (permissions: string[]) => boolean,
   selectedCompany?: MeCompany,
 ) {
-  return Boolean(
-    user.type === 'PERSON' &&
-      (selectedCompany?.is_manager_competence ||
-        selectedCompany?.is_manager_in_check ||
-        checkPermission(['managers_vacations_list']) ||
-        checkPermission(['managers_list_occurrences']))
-  );
+  const isPerson = user.type === 'PERSON';
+  const isManagerCompetence = selectedCompany?.is_manager_competence;
+  const isManagerInCheck = selectedCompany?.is_manager_in_check;
+  const hasVacationsPermission = checkPermission(['managers_vacations_list']);
+  const hasOccurrencesPermission = checkPermission(['managers_list_occurrences']);
+  return isPerson && (isManagerCompetence || isManagerInCheck || hasVacationsPermission || hasOccurrencesPermission);
 }
