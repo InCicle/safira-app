c870835d0a7a70ae8bf8a20eb370ffb6fdcc3d3d
gmendesfonseca <fonseca.gabriel.mendes@gmail.com>
2025-06-13 15:11:07 -0300

    fix: 🐛 add optional chaining for companies length check in InHeader component

--- interfaces/Language.ts
+++ interfaces/Language.ts
@@ -1,6 +1,8 @@
-export enum LanguageType {
-  'pt-BR' = 'pt-BR',
-  'en-US' = 'en-US',
-  en = 'en',
-  pt = 'pt',
-}
+export const LanguageType = {
+  'pt-BR': 'pt-BR',
+  'en-US': 'en-US',
+  en: 'en',
+  pt: 'pt',
+} as const
+
+export type LanguageType = (typeof LanguageType)[keyof typeof LanguageType];
