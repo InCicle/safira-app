863211e2af1f2818c3cb2a53a40445c127cf7547
gmendesfonseca <fonseca.gabriel.mendes@gmail.com>
2025-07-17 11:16:00 -0300

    refactor: :recycle: simplify usePermissions hook by removing unnecessary refetch logic

--- src/hooks/usePermissions.ts
+++ src/hooks/usePermissions.ts
@@ -1,10 +1,5 @@
-import { useEffect } from 'react';
 import { useGetPermissionsQuery } from '../services/api/permissions';
 
 export function usePermissions() {
-  const { refetch } = useGetPermissionsQuery();
-
-  useEffect(() => {
-    refetch();
-  }, [refetch]);
+  return useGetPermissionsQuery();
 }
